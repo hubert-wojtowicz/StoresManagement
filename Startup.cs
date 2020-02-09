5a1c08e739b8ff7d372c4d1cc3d53f35b878481d
FabloAlencar <57524853+FabloAlencar@users.noreply.github.com>
2020-02-09 19:29:48 +0100

    C01: Inital Model

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StoresManagement.Startup))]
+namespace StoresManagement
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
